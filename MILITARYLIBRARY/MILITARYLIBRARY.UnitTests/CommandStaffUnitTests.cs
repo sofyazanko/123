6529b85bc408a3d0053aac034f7e07b6302a628c
София Занько <sofya_zanko@mail.ru>
2026-05-09 14:48:25 +0500

    Задание 14 выполнено (все тесты пройдены)

---
+++ MILITARYLIBRARY/MILITARYLIBRARY.UnitTests/CommandStaffUnitTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MILITARYLIBRARY;
+
+namespace MILITARYLIBRARY.UnitTests
+{
+        [TestFixture]             
+        public class CommandStaffUnitTests
+        {
+            [Test]
+            public void ConstructorTest()
+            {
+                var staff = CreateTestCommandStaff();//создаем военнослуж
+
+                Assert.That(staff.Name, Is.EqualTo("Алексей"));
+                Assert.That(staff.Surname, Is.EqualTo("Алексеев"));
+                Assert.That(staff.UnitName, Is.EqualTo("1я рота"));
+                Assert.That(staff.Position, Is.EqualTo("Командир роты"));
+            }
+
+            [Test]
+            public void GetInfoTest()
+            {
+                var staff = CreateTestCommandStaff();
+                var info = staff.GetInfo();
+
+                
+                Assert.That(info.Length, Is.EqualTo(3));//проверяем кол-во строк
+                Assert.That(info[0], Is.EqualTo("Алексей Алексеев"));//проверяем построчно
+                Assert.That(info[1], Does.Contain("АБВ123"));
+                Assert.That(info[2], Does.Contain("Командный состав"));
+                Assert.That(info[2], Does.Contain("1я рота"));
+                Assert.That(info[2], Does.Contain("Командир роты"));
+            }
+
+            private CommandStaff CreateTestCommandStaff()//создаем заготовку
+            {
+                return new CommandStaff("Алексей", "Алексеев", "АБВ123",
+                                       TypeService.UnderContract, "1я рота", "Командир роты");
+            }
+        }
+    }
---
+++ MILITARYLIBRARY/MILITARYLIBRARY.UnitTests/VeteranUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MILITARYLIBRARY;
+
+namespace MILITARYLIBRARY.UnitTests
+{
+    [TestFixture]
+    public class VeteranUnitTests
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var veteran = CreateTestVeteran();//созд ветерана
+
+            Assert.That(veteran.Name, Is.EqualTo("Петр"));
+            Assert.That(veteran.Surname, Is.EqualTo("Петров"));
+            Assert.That(veteran.MilitaryIDN, Is.EqualTo("ОСА111"));
+            Assert.That(veteran.Type, Is.EqualTo(TypeService.Urgent));
+            Assert.That(veteran.YearsOfService, Is.EqualTo(25));
+            Assert.That(veteran.PensionAmount, Is.EqualTo(35000m));
+        }
+
+        [Test]
+        public void GetInfoTest()
+        {
+            var veteran = CreateTestVeteran();
+            var info = veteran.GetInfo();
+
+            Assert.That(info.Length, Is.EqualTo(3));
+            Assert.That(info[0], Is.EqualTo("Петр Петров"));
+            Assert.That(info[1], Does.Contain("ОСА111"));
+            Assert.That(info[2], Does.Contain("Ветеран"));
+            Assert.That(info[2], Does.Contain("25 лет"));
+            Assert.That(info[2], Does.Contain("35000"));
+        }
+
+        private Veteran CreateTestVeteran()
+        {
+            return new Veteran("Петр", "Петров", "ОСА111",
+                              TypeService.Urgent, 25, 35000m);
+        }
+    }
+}
